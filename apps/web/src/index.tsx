0a410d0819749352463b999378f55ca6f7ecbd03
Rajveer Singh <therajveersingh@icloud.com>
2026-08-25 00:45:54 +0530

    Add particle field background across web pages

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import { ParticleField } from "./components/ParticleField";
 import { InvitationUpload } from "./pages/InvitationUpload";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
@@ -14,6 +15,7 @@ if (!root) throw new Error("Root element not found");
 
 createRoot(root).render(
 	<StrictMode>
+		<ParticleField />
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />} />
